26f0742a4756f133db9291580b009e45a47eec34
Johan Smarius <jsmarius@hotmail.com>
2021-02-05 10:39:07 +0100

    Initial version

---
+++ TestSonar/TestSonar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestSonar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
